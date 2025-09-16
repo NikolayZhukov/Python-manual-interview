b32acbb9c360fca18e8565ed8ec7501043331936
nikolay <nikolayzh86@gmail.com>
2025-09-16 20:50:36 +0300

    3

---
+++ tests/test_data_types/dictionaries.py
@@ -0,0 +1,24 @@
+# cars = {'1': 'black', '2': 'white', '3': 'red'}
+# for k in cars.items():
+#     print(k)
+#
+# cars = {'1': 'black', '2': 'white', '3': 'red'}
+# for k in cars.keys():
+#     print(k)
+#
+# cars = {'1': 'black', '2': 'white', '3': 'red'}
+# for k in cars.values():
+#     print(k)
+from venv import create
+
+# cars = {'1': 'black', '2': 'white', '3': 'red'}
+# print(cars['2'])
+
+# cars = {'1': 'black', '2': 'white', '3': 'red'}
+# cars['2'] = 'blue'
+# print(cars)
+#
+# cars = {'1': 'black', '2': 'white', '3': 'red'}
+# del(cars['2'])
+# print(cars)
+
