280fb56859d92f8ac0ea3cc04618c2611533ad36
nikolay <nikolayzh86@gmail.com>
2025-09-23 18:14:52 +0300

    operations

--- tests/variables_operators.py
+++ tests/variables_operators.py
@@ -1,24 +1,24 @@
-# a = [5]
-# b = [5]
-# print(id(a))
-# print(id(b))
+a = [5]
+b = [5]
+print(id(a))
+print(id(b))
 
-# a = (5555.1,)
-# b = '5555'
-# # print(id(a))
-# # print(id(b))
-#
-# print(type(a))
-# print(type(b))
+a = (5555.1,)
+b = '5555'
+print(id(a))
+print(id(b))
 
-# c = 7 // 2
-# print(c)
-#
-# d = -7 // 2
-# print(d)
+print(type(a))
+print(type(b))
 
-# e = 10 % 3
-# print(e)
+c = 7 // 2
+print(c)
+
+d = -7 // 2
+print(d)
+
+e = 10 % 3
+print(e)
 
 i = 3
 i += 1
@@ -35,3 +35,27 @@ print(g)
 h = 3
 h /= 3
 print(h)
+
+
+x = 10  # глобальная переменная
+
+def change_x():
+    global x      # говорим, что будем использовать глобальную переменную x
+    x = 20        # изменяем глобальную x
+
+print("До вызова функции:", x)  # 10
+change_x()
+print("После вызова функции:", x)  # 20
+
+
+def outer():
+    x = 10  # переменная во внешней функции
+
+    def inner():
+        nonlocal x   # используем x из outer(), а не создаём новую локальную
+        x = 20       # изменяем x из внешней функции
+
+    inner()
+    print("Значение x во внешней функции:", x)
+
+outer()
