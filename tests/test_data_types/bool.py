d9cf8be3d094caaca39533ea96cdfd323103f3f1
nikolay <nikolayzh86@gmail.com>
2025-09-23 23:59:32 +0300

    operations

---
+++ tests/test_data_types/bool.py
@@ -0,0 +1,21 @@
+# print(4 > 9)
+
+# print(type(4 <= 9))
+
+# print(4 >= 9)
+
+# a = True
+# print(type(a))
+
+# a = 8.97
+# print(a >= -4 and a <= 5)
+
+a = 2.97
+print(a < -4 or a > 5)
+
+'''Инвертирование общего составного условия'''
+a = 2.97
+print(not(a < -4 or a > 5))
+
+
+
--- tests/test_data_types/lists.py
+++ tests/test_data_types/lists.py
@@ -1,3 +1,32 @@
+b = [1, 5, 88, [21, 13], 5, 77]
+print(b.index(5, 3))
+
+
+# a = [2, 4, 99]
+# b = a[:]
+# print(b)
+
+# a = [2, 4, 99]
+# b = (a.copy())
+# print(id(b))
+# print(id(a))
+
+# b = [1, 5, 88, [21, 13]]
+# cut = b.pop(0)
+# print(cut)
+
+
+# t = [4.5, 2, 7, 8, 16]
+# print(len(t))
+# print(sum(t))
+# average = sum(t) / len(t)
+# print(average)
+
+# c = [2, 4, 99]
+# c2 = list(c)
+# print(id(c))
+# print(id(c2))
+
 # import pytest
 #
 
@@ -16,14 +45,14 @@
 # even_squares = [x * x for x in range(100) if x % 2 == 0]
 # print(even_squares)
 
-squares = (x * x for x in range(10))
-print(squares)         # <generator object ...>
-print(next(squares))   # 0
-print(next(squares))  # 1
-
-sum = 155
-sum_repr =
-print()
+# squares = (x * x for x in range(10))
+# print(squares)         # <generator object ...>
+# print(next(squares))   # 0
+# print(next(squares))  # 1
+#
+# sum = 155
+# sum_repr =
+# print()
 
 
 # def search_python_tutorials():
