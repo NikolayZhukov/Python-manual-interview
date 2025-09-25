b7b4a8bdf69936c1daff0e8683f6aed48e901af4
nikolay <nikolayzh86@gmail.com>
2025-09-25 21:34:00 +0300

    operations

---
+++ tests/cycles/for.py
@@ -0,0 +1,30 @@
+cities = input().split()
+
+for i in range(len(cities)):
+    cities[i]=len(cities[i])
+    print(cities[i], end=' ')
+
+
+# n = list(map(int, input().split()))
+# sum = 0
+# for i in n:
+#     sum += i if i % 2 == 1 else 0
+# print(sum)
+
+
+
+# for i in range(1, 20, 3):
+#     print(i, end=' ')
+
+
+
+# for x in range(-10, -1):
+#     if x % 2 == 0:
+#         print(x, end=' ')
+
+
+# for x in range(11):
+#     print(x, end=' ')
+#
+# for x in range(-10, 1):
+#     print(x, end=' ')
