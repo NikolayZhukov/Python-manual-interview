e88ec6d0b4bf3118af5314fd1f3e5ee58f17ac6a
nikolay <nikolayzh86@gmail.com>
2025-09-30 15:57:32 +0300

    operations

--- tests/functions/functions_basics.py
+++ tests/functions/functions_basics.py
@@ -1,13 +1,106 @@
-def even(y):
-    return y % 2 == 0   # True, если чётное; False, если нечётное
+digs = list(map(int, input().split()))
+a1 = max(digs)
+b1 = min(digs)
 
-# запускаем цикл ввода
-while True:
-    x = int(input())    # читаем число
-    if x == 1:          # условие выхода
-        break
-    if even(x):         # проверка через функцию
-        print(x)
+def multiply(a, b):
+    sum = a * b
+    return sum
+
+print(multiply(a1, b1))
+
+
+# cities = input().split()
+#
+# def get_cities(value):
+#     length_value = len(value)
+#     return value, length_value   # кортеж (город, длина)
+#
+# # словарь: ключ = город, значение = его длина
+# d = {city: get_cities(city)[1] for city in cities}
+#
+# # сортировка ключей по значениям (длине города)
+# a = sorted(d, key=d.get)
+# print(*a)
+
+
+
+# cities = input().split()
+#
+# def filter_cities(x):
+#     if len(x) >= 6:
+#         return True
+#
+# lst = [city for city in cities if filter_cities(city)]
+# print(*lst)
+
+
+
+# tp = input().strip()
+#
+# if tp == 'RECT':
+#     def get_sq(a, b):
+#         return a * b
+# else:
+#     def get_sq(a):
+#         return a * a
+
+
+# tp = input().strip()
+# len_input = True if tp == 'RECT' else False
+#
+# def get_sq(a, b):
+#     return a * b if len_input else a ** 2
+
+# print(get_sq())
+
+
+
+# def odd(x):
+#     return x % 2 == 1   # True, если нечетное; False, если четное
+#
+# lst_d = list(map(int, input().split()))
+#
+# lst = [num for num in lst_d if odd(num)]
+#
+# print(*lst)
+#
+#
+# def odd(x):
+#     return x % 2 == 1   # True, если нечетное; False, если четное
+#
+# lst_d = list(map(int, input().split()))
+#
+# lst = []
+# for num in lst_d:       # перебираем все элементы
+#     if odd(num):        # проверяем на нечетность
+#         lst.append(num) # добавляем в новый список
+#
+# print(*lst)
+
+
+# def not_even(y):
+#     return y % 2 == 1
+# lst_d = list(map(int, input().split()))
+# lst = []
+#
+# for x in lst_d:
+#     if not_even(x):
+#         lst.append(x)
+# print(*lst)
+
+
+
+
+# def even(y):
+#     return y % 2 == 0   # True, если чётное; False, если нечётное
+#
+# # запускаем цикл ввода
+# while True:
+#     x = int(input())    # читаем число
+#     if x == 1:          # условие выхода
+#         break
+#     if even(x):         # проверка через функцию
+#         print(x)
 
 # def is_large(input_str):
 #     return False if len(input_str) < 3 else True
