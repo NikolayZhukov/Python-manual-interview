9edd318931f5194916fbdaaf38f509631b25ad31
nikolay <nikolayzh86@gmail.com>
2025-10-01 22:22:38 +0300

    operations

---
+++ tests/various/try.py
@@ -0,0 +1,36 @@
+
+
+# def division():
+#     try:
+#         a, b = map(int, input().split())
+#         c = a / b
+#     except ValueError:
+#         print('Введите именно число, а не букву!')
+#     except ZeroDivisionError:
+#         print('Введите любое число кроме ноля! На ноль делить нельзя!')
+#     else:
+#         return c
+#     finally:
+#         print('Ждём вас снова!')
+#
+# division()
+
+
+# try:
+#     f = open("data.txt", "r")
+#     content = f.read()
+# except FileNotFoundError:
+#     print("Файл не найден")
+# except PermissionError:
+#     print("Нет доступа к файлу")
+
+
+
+# try:
+#     x = int("2d2")   # вызовет ValueError
+# except ValueError:
+#     print("Неверное преобразование строки в число")
+# else:
+#     print("Преобразование прошло успешно", x)
+# finally:
+#     print("Закрываю ресурс")
