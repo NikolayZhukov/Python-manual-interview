e4b00d413fbe56b69faab07345556f010ab4389c
nikolay <nikolayzh86@gmail.com>
2025-10-03 14:04:34 +0300

    werw

---
+++ tests/oop/abstractia.py
@@ -0,0 +1,25 @@
+from abc import ABC, abstractmethod
+
+class Book(ABC):
+    def __init__(self, title, author):
+        self.title = title
+        self.author = author
+
+    @abstractmethod
+    def get_summary(self):
+        pass
+
+class Fiction(Book):
+    def get_summary(self):
+        print(f'"{self.title}" - роман в стиле исторический фикшн, автор - {self.author}')
+
+class NonFiction(Book):
+    def get_summary(self):
+        print(f'"{self.title}" - книга в стиле нон фикшн, автор - {self.author}')
+
+class Poetry(Book):
+    pass
+
+
+book1 = Fiction('Будни', 'Петров')
+print(book1.get_summary())
--- tests/oop/nasledovanie.py
+++ tests/oop/nasledovanie.py
@@ -5,19 +5,47 @@ class Animal:
     def speak(self):
         raise NotImplementedError("Subclass must implement abstract method")
 
-class Dog(Animal):
+class Cat:
     def speak(self):
-        return f"{self.name} says Woof!"
+        return "Meow"
 
-class Cat(Animal):
+class Dog:
     def speak(self):
-        return f"{self.name} says Meow!"
+        return "Woof"
 
-dog = Dog("Бобик")
-cat = Cat("Маруся")
+def make_animal_speak(animal):
+    print(animal.speak())
 
+cat = Cat()
+dog = Dog()
+
+make_animal_speak(cat)  # Вывод: Meow
+make_animal_speak(dog)  # Вывод: Woof
 print(dog.speak())  # Buddy says Woof!
 print(cat.speak())  # Whiskers says Meow!
+
+
+
+# class Animal:
+#     def __init__(self, name):
+#         self.name = name
+#
+#     def speak(self):
+#         raise NotImplementedError("Subclass must implement abstract method")
+#
+# class Dog(Animal):
+#     def speak(self):
+#         return f"{self.name} says Woof!"
+#
+# class Cat(Animal):
+#     def speak(self):
+#         return f"{self.name} says Meow!"
+#
+# dog = Dog("Бобик")
+# cat = Cat("Маруся")
+#
+# print(dog.speak())  # Buddy says Woof!
+# print(cat.speak())  # Whiskers says Meow!
 
 
 
