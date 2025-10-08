aaa13db9682a421d76a9e4a8c8670d4855a77a72
nikolay <nikolayzh86@gmail.com>
2025-10-08 14:48:48 +0300

    longest word

--- tests/interview_problems/for.py
+++ tests/interview_problems/for.py
@@ -61,13 +61,31 @@ words = ["яблоко", "кот", "программирование", "соба
 То же самое с помощью генератора списков
 """
 
-def find_longest_words(words):
-    words_sorted = sorted(words, key=len, reverse=True)
-    longest_word = words_sorted[0]
-    words_longest_lst = [word for word in words_sorted if word == longest_word]
-    return words_longest_lst
+# def find_longest_words(words):
+#     words_sorted = sorted(words, key=len, reverse=True)
+#     longest_word = words_sorted[0]
+#     words_longest_lst = [word for word in words_sorted if word == longest_word]
+#     return words_longest_lst
+#
+# words = ["яблоко", "кот", "программирование", "собака", "электричество"]
+# print(find_longest_words(words))
 
+
+"""
+1.4. Имеется список слов:
 words = ["яблоко", "кот", "программирование", "собака", "электричество"]
-print(find_longest_words(words))
+Создайте новый список, в котором вместо каждого слова будет указано количество букв в данном слове.
+"""
+# words = ["яблоко", "кот", "программирование", "собака", "электричество"]
+# words_len = []
+# for word in words:
+#     words_len.append(len(word))
+#
+# print(words_len)
 
-
+"""
+То же самое с помощью генератора списков
+"""
+# words = ["яблоко", "кот", "программирование", "собака", "электричество"]
+# words_len = [len(word) for word in words]
+# print(words_len)
