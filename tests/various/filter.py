f797f9b6573df4a15a974aba2df203d24dbda516
nikolay <nikolayzh86@gmail.com>
2025-11-01 11:01:03 +0300

    Исправлено умножение на деление и 6 на 1

--- tests/various/filter.py
+++ tests/various/filter.py
@@ -1,15 +1,20 @@
+""""""
+"""filter"""
 numbers = [1, 2, 3, 4, 5, 6]
 even = filter(lambda x: x % 2 == 0, numbers)
 print(list(even))
 
+"""sorted + filter"""
 numbers = [1, 2, 3, 4, 5, 6]
 even = sorted(filter(lambda x: x % 2 == 0, numbers))
 print(even)
 
+"""sorted"""
 numbers = [1, 2, 3, 4, 5, 6]
 even = sorted([x for x in numbers if x % 2 == 0])
 print(even)
 
+"""sort"""
 numbers = [1, 2, 3, 4, 5, 6]
 even = [x for x in numbers if x % 2 == 0]
 even.sort()
