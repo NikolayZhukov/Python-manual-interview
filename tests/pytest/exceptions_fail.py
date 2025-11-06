b99d816be38dc191c205df4113b0412974e8ace0
nikolay <nikolayzh86@gmail.com>
2025-11-06 18:45:58 +0300

    Исправлено умножение на деление и 6 на 1

--- tests/pytest/exceptions_fail.py
+++ tests/pytest/exceptions_fail.py
@@ -14,26 +14,32 @@ import pytest
 #         raise ValueError("Деление на ноль не допускается")
 #     return a / b
 #
-# print(divide(8,0))
+# print(divide(8,1))
 
 
 # # БАЗОВЫЙ PYTHON
-# def divide(a, b):
-#     try:
-#         return a / b
-#     except ZeroDivisionError:
-#         return "Ошибка: Деление на ноль не допускается"
-#
-# print(divide(8, 0))
+def divide(a, b):
+    try:
+        result = a / b
+    except ZeroDivisionError:
+        return "Ошибка: Деление на ноль не допускается"
+    else:
+        print(result)
+        return result
+
+divide(8, 2)
+
+
+
 
 
 # # PYTEST
-def divide(a, b):
-    if b == 0:
-        raise ValueError("Деление на ноль не допускается")
-    return a / b
-
-# Тест с использованием pytest.raises
-def test_divide_by_zero():
-    with pytest.raises(ValueError):
-        divide(10, 0)
+# def divide(a, b):
+#     if b == 0:
+#         raise ValueError("Деление на ноль не допускается")
+#     return a / b
+#
+# # Тест с использованием pytest.raises
+# def test_divide_by_zero():
+#     with pytest.raises(ValueError):
+#         divide(10, 0)
