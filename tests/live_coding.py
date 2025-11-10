ae135468fbe141e7abdd19b40e310dd503f57969
nikolay <nikolayzh86@gmail.com>
2025-11-10 17:37:46 +0300

    longest word

--- tests/live_coding.py
+++ tests/live_coding.py
@@ -1,30 +1,18 @@
 """"""
 """
-Программа загадывает случайное число от 1 до 10. 
-Пользователь должен угадать его. 
-На каждой попытке программа подсказывает, больше или меньше названное число загаданного. 
-Цикл продолжается, пока число не будет угадано. 
-В конце выводится количество попыток.
+ Программа запрашивает у пользователя слова до тех пор, пока он не введет слово "стоп", регистронезависимо. 
+ После этого выводится количество введенных слов и сами слова.
 """
-secret_number = 7
-attempts = 0
+words = []
 while True:
-    attempts += 1
-    try:
-        user_guess = int(input('Введите целое число: '))
-        if user_guess < secret_number:
-            print('Больше!')
-        else:
-            print('Меньше!')
-    except ValueError:
-        print('Ошибка')
-        continue
+    user_input = input()
+    if user_input.lower() == "стоп":
+        break
     else:
-        if user_guess == secret_number:
-            break
+        words.append(user_input)
 
-print(f'Угадал! Секретное число = {secret_number}')
-print(f'{attempts} попытки!')
+print(len(words), *words)
+
 
 
 
