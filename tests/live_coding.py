1bfbf471dc0e3b5af2d9bc709686bc603052a57e
nikolay <nikolayzh86@gmail.com>
2025-11-11 14:07:25 +0300

    Исправлено умножение на деление и 6 на 1

--- tests/live_coding.py
+++ tests/live_coding.py
@@ -1,17 +1,27 @@
 """"""
-"""
- Программа запрашивает у пользователя слова до тех пор, пока он не введет слово "стоп", регистронезависимо. 
- После этого выводится количество введенных слов и сами слова.
-"""
-words = []
-while True:
-    user_input = input()
-    if user_input.lower() == "стоп":
-        break
-    else:
-        words.append(user_input)
+def decorator_with_arguments(show_before, show_after):
+    def greet_str_decorator(func):
+        def wrapper(*args, **kwargs):
+            text = func(*args, **kwargs)
+            if show_before:
+                print('До декорируемой функции')
+            result = f'Привет, {text}'
+            print(f'Result = {result}')
+            if show_after:
+                print('После декорируемой функции')
+            return result
+        return wrapper
+    return greet_str_decorator
 
-print(len(words), *words)
+@decorator_with_arguments(show_before=False, show_after=False)
+def get_string(name, surname):
+    return f'{name} {surname}'
+
+name = input('Введите имя: ')
+get_string(name, surname='Петров')
+
+
+
 
 
 
