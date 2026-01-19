6ed4eca040914d94bf1cc509c450f4daca31d586
nikolay <nikolayzh86@gmail.com>
2026-01-19 16:20:41 +0300

    Requests POST

--- tests/requests/get.py
+++ tests/requests/get.py
@@ -8,11 +8,29 @@ id = '333125'
 
 def find_pet_by_id():
     url = f'{base_url}{id}'
-
     response = requests.get(url)
-    print(response.json())
-    print(url)
-    assert response.status_code == 200
+
+    payload = response.json()
+
+    # print(f'request = {response.request}')
+    # print(f'json = {response.json()}')
+    # print(f'status code = {response.status_code}')
+    # print(f'text = {response.text}')
+    # print(f'content = {response.content}')
+    # print(f'headers = {response.headers}')
+    # print(f'url = {response.url}')
+    # print(f'elapsed time = {response.elapsed}')
+    # print(f'encoding = {response.encoding}')
+    # print(f'cookies = {response.cookies}')
+
+    print(payload)
+    # print(f'payload_id: {payload["id"]}')
+    # print(f'payload_name: {payload["name"]}')
+    # print(f'payload_tags: {payload["tags"]}')
+    print(f'payload_tags_id: {payload["tags"][0]["id"]}')
+    print(f'payload_category_id: {payload["category"]["id"]}')
+
+    # assert response.status_code == 200
 
 if __name__ == "__main__":
     find_pet = find_pet_by_id()
