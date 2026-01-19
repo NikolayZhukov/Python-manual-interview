6553d4ee1979eddef6347f6cc47748fbb68fdcd4
nikolay <nikolayzh86@gmail.com>
2026-01-19 18:19:12 +0300

    Requests POST

--- tests/requests/class_requests.py
+++ tests/requests/class_requests.py
@@ -25,6 +25,7 @@ class TestPetStoreRequests():
         }
 
         response = requests.post(self.url, json=body)
+        print(f'\nrequest_body: {body}')
         assert response.status_code == 200
         print('\nResponse JSON:', response.json())
 
@@ -35,8 +36,57 @@ class TestPetStoreRequests():
         }
 
         response = requests.post(self.url, data=data)
-        print(f'\nupdate_form_data: {response.json()}')
+        print(f'\nrequest_data: {data}')
         assert response.status_code == 200
+
+    def test_find_pet_by_id(self):
+        self.url = f'{BASE_URL}{pet_id}'
+        response = requests.get(self.url)
+
+        payload = response.json()
+
+        # print(f'request = {response.request}')
+        # print(f'json = {response.json()}')
+        # print(f'status code = {response.status_code}')
+        # print(f'text = {response.text}')
+        # print(f'content = {response.content}')
+        # print(f'headers = {response.headers}')
+        # print(f'url = {response.url}')
+        # print(f'elapsed time = {response.elapsed}')
+        # print(f'encoding = {response.encoding}')
+        # print(f'cookies = {response.cookies}')
+
+        print(f'payload: {payload}')
+        # print(f'payload_id: {payload["id"]}')
+        # print(f'payload_name: {payload["name"]}')
+        # print(f'payload_tags: {payload["tags"]}')
+        # print(f'payload_tags_id: {payload["tags"][0]["id"]}')
+        # print(f'payload_category_id: {payload["category"]["id"]}')
+
+        assert response.status_code == 200
+        # return f'payload_category_id: {payload["category"]["id"]}'
+
+    def test_delete_pet_by_id(self):
+        self.url = f'{BASE_URL}{pet_id}'
+        response = requests.delete(self.url)
+        payload = response.json()
+        print(response.json())
+        print(f'message: {payload["message"]}')
+
+    def test_find_pet_by_id_after_delete(self):
+        self.url = f'{BASE_URL}{pet_id}'
+        response = requests.get(self.url)
+
+        payload = response.json()
+
+        print(f'payload: {payload}')
+        print(f'message: {payload["message"]}')
+
+
+        assert response.status_code == 404
+        assert payload["message"] == "Pet not found"
+        # return f'payload_category_id: {payload["category"]["id"]}'
+
 
 
 
